aa2548489551e9647d8009567c2433921966f8c5
Oleksii Horodetskyi <oleksii.horodetskyi@rhinocodes.com>
2024-02-13 16:47:00 +0200

    modal

---
+++ src/components/modal/modal.component.jsx
@@ -0,0 +1,10 @@
+import {} from './modal.styles';
+
+const Modal = () => {
+    return (
+        <h1></h1>
+    )
+}
+
+
+export default Modal;
--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -31,19 +31,19 @@ const SignInForm = () => {
     event.preventDefault();
 
     try {
-      await signInAuthUserWithEmailAndPassword;
+      await signInAuthUserWithEmailAndPassword(email, password);
       resetFormFields();
+
+      alert('Successfully authenticated');
     } catch (error) {
       switch (error.code) {
-        case "auth/wrong-password":
-          alert("incorrect password for email");
-          break;
-        case "auth/user-not-found":
-          alert("no user associated with this email");
+        case "auth/invalid-login-credentials":
+          alert('User sign in failed');
           break;
         default:
           console.log(error);
       }
+      console.log('user sign in failed', error);
     }
   };
 
