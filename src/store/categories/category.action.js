153a5da78ac2ae917fa0e9abb5f09503967e938a
Oleksii Horodetskyi <oleksii.horodetskyi@rhinocodes.com>
2024-03-09 19:25:05 +0200

    build redux: redux-thunk

--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,5 +1,25 @@
 import { CATEGORIES_ACTION_TYPES } from "./category.type";
 import { createAction } from "../../utils/reducer/reducer.utils";
 
-export const setCategories = (categories) =>
-  createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES, categories);
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
+
+export const fetchCategoriesStart = () =>
+  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START);
+
+export const fetchCategoriesSuccess = (categories) =>
+  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS, categories);
+
+export const fetchCategoriesFailure = (error) =>
+  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
+
+export const fetchCategoriesStartAsync = () => {
+  return async (dispatch) => {
+    dispatch(fetchCategoriesStart());
+    try {
+      const categoriesArray = await getCategoriesAndDocuments("categories");
+      dispatch(fetchCategoriesSuccess(categoriesArray));
+    } catch (error) {
+      dispatch(fetchCategoriesFailure(error));
+    }
+  };
+};
--- src/store/store.js
+++ src/store/store.js
@@ -2,25 +2,32 @@ import { compose, createStore, applyMiddleware } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import logger from "redux-logger";
+import { thunk } from "redux-thunk";
+import { rootReducer } from './root-reducer';
 
-import { rootReducer } from "./root-reducer";
+const middleWares = [
+  process.env.NODE_ENV === 'development' && logger,
+  thunk,
+].filter(Boolean);
+
+const composeEnhancer =
+  (process.env.NODE_ENV !== 'production' &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
 
 const persistConfig = {
-  key: "root",
+  key: 'root',
   storage,
-  blacklist: ["user"],
+  whitelist: ['cart'],
 };
 
-const persistedReduser = persistReducer(persistConfig, rootReducer);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [process.env.NODE_ENV === '[production]' && logger].filter(Boolean);
-
-const composeEnhancer = (process.env.NODE_ENV === '[production]' && window && window.__REDUX_DEVLTOOLS_EXPENSION_COMPOSE__)
-
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 export const store = createStore(
-  persistedReduser,
+  persistedReducer,
   undefined,
   composedEnhancers
 );
